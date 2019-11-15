de95d4efe0c7bce8bbb1a7495fb943e8bf639b2c
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2019-11-15 11:31:07 +0530

    main queue(serial), global queue(concurrent), custom queue (serial, concurrent )

--- ThreadingAndConcurrency/ThreadClass.swift
+++ ThreadingAndConcurrency/ThreadClass.swift
@@ -16,22 +16,63 @@ class ThreadClass: NSObject {
     }
     
     func createGCD()  {
-        DispatchQueue.main.async {
-            print("A")
-            DispatchQueue.main.async {
-                print("B")
+        
+        Thread.printCurrent()
+        
+        if !Thread.current.isMainThread {
+            DispatchQueue.main.sync {
+                print("A")
             }
-            DispatchQueue.main.async {
-                print("C")
-            }
-        }
-        DispatchQueue.main.async {
-            print("D")
         }
         
-        DispatchQueue.global(qos: .utility).async {
+        
+        DispatchQueue.main.async {
+            print("B")
+            
+//            DispatchQueue.main.sync {
+//                print("C")
+//            }
+            
+            DispatchQueue.main.async {
+                print("D")
+            }
+        }
+        
+        DispatchQueue.main.async { // always serial queue
+            Thread.printCurrent()
             print("E")
+        }
+                
+        DispatchQueue.global(qos: .utility).async {  // always Concurrent Queue
+            Thread.printCurrent()
+            DispatchQueue.main.sync {
+                  print("F")
+            }
+            print("G")
+        }
+        
+        DispatchQueue(label: "SunilKumar").async { // serial queue
+            Thread.printCurrent()
+               DispatchQueue.main.sync {
+                     print("H")
+               }
+        }
+        
+        
+        let queue = DispatchQueue(label: "Sunil", qos: .background, attributes: .concurrent, autoreleaseFrequency: .inherit, target: nil)
+        
+        queue.async {  //  concurrent queue
+            Thread.printCurrent()
+            DispatchQueue.main.sync {
+                print("I")
+            }
         }
     }
 
+}
+
+extension Thread {
+    class func printCurrent() {
+        print("\r⚡️: \(Thread.current)\r" + "🏭: \(OperationQueue.current?.underlyingQueue?.label ?? "None")\r")
+    }
 }
